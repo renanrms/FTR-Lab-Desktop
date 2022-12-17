0d0e06dd97668eeb9246a3ceda30b7bfc127e142
Renan Passos <renanpassos@poli.ufrj.br>
2022-12-17 18:52:57 -0300

    Começa pelo projeto do curso da rocketseat no final da etapa do setup da aplicação.

---
+++ src/renderer/tailwind.config.js
@@ -0,0 +1,25 @@
+const plugin = require('tailwindcss/plugin')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.tsx'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: 'Inter, sans-serif',
+      },
+    },
+  },
+  plugins: [
+    plugin(({ addUtilities }) => {
+      addUtilities({
+        '.region-drag': {
+          '-webkit-app-region': 'drag',
+        },
+        '.region-no-drag': {
+          '-webkit-app-region': 'no-drag',
+        },
+      })
+    }),
+  ],
+}
