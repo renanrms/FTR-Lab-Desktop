d7e6e49f9d671518321dcfc9bf978b4a65403c09
Renan Passos <renanpassos@poli.ufrj.br>
2023-01-10 22:25:23 -0300

    Aplica componente responsive container e retira os hooks.

--- src/renderer/src/features/chart/components/Chart.tsx
+++ src/renderer/src/features/chart/components/Chart.tsx
@@ -1,4 +1,3 @@
-import React, { useLayoutEffect, useRef, useState } from 'react'
 import {
   LineChart,
   Line,
@@ -7,6 +6,7 @@ import {
   CartesianGrid,
   Tooltip,
   Legend,
+  ResponsiveContainer,
 } from 'recharts'
 
 interface ChartPropType {
@@ -16,52 +16,16 @@ interface ChartPropType {
   data: Object[]
 }
 
-function useWindowSize() {
-  const [size, setSize] = useState([0, 0])
-  useLayoutEffect(() => {
-    function updateSize() {
-      setSize([window.innerWidth, window.innerHeight])
-    }
-    window.addEventListener('resize', updateSize)
-    updateSize()
-    return () => window.removeEventListener('resize', updateSize)
-  }, [])
-  return size
-}
-
-// function useComponentSize<T>(ref: React.RefObject<T>) {
-//   const [size, setSize] = useState([0, 0])
-//   useLayoutEffect(() => {
-//     function updateSize() {
-//       setSize([ref.current?.offsetWidth || 0, ref.current?.offsetHeight || 0])
-//     }
-//     ref.current.addEventListener('resize', updateSize)
-//     updateSize()
-//     return () => ref.current.removeEventListener('resize', updateSize)
-//   }, [])
-//   return size
-// }
-
 export function Chart(props: ChartPropType) {
-  const chartContainerRef = useRef<HTMLDivElement>(null)
-
-  const width = chartContainerRef?.current?.offsetWidth || 500
-  const height = chartContainerRef?.current?.offsetHeight || 250
-
-  useWindowSize()
-
   return (
     <div
       className={[
-        'p-8 shadow border-2 border-secondary-90 rounded-lg',
+        'p-8 shadow border-2 border-secondary-90 bg-surface rounded-lg',
         props.className,
       ].join(' ')}
     >
-      <div
-        className="w-full h-full rounded bg-neutral-95"
-        ref={chartContainerRef}
-      >
-        <LineChart width={width} height={height}>
+      <ResponsiveContainer width="100%" height="100%" debounce={20}>
+        <LineChart width={200} height={200}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey={props.XAxis.key} type="number" />
           <YAxis dataKey={props.YAxis.key} />
@@ -73,7 +37,7 @@ export function Chart(props: ChartPropType) {
             name={props.YAxis.name}
           />
         </LineChart>
-      </div>
+      </ResponsiveContainer>
     </div>
   )
 }
