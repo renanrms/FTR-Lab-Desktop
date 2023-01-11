1fe7d914241d97bb7ed4c6f7ecd8719585e13e2e
Renan Passos <renanpassos@poli.ufrj.br>
2023-01-11 18:51:47 -0300

    Adiciona botões de controlde do gráfico.

--- src/renderer/src/features/chart/components/Chart.tsx
+++ src/renderer/src/features/chart/components/Chart.tsx
@@ -1,3 +1,8 @@
+import ExpandRoundedIcon from '@mui/icons-material/ExpandRounded'
+import VerticalAlignBottomRoundedIcon from '@mui/icons-material/VerticalAlignBottomRounded'
+import ScatterPlotRoundedIcon from '@mui/icons-material/ScatterPlotRounded'
+import ShowChartRoundedIcon from '@mui/icons-material/ShowChartRounded'
+
 import {
   LineChart,
   Line,
@@ -5,7 +10,6 @@ import {
   YAxis,
   CartesianGrid,
   Tooltip,
-  Legend,
   ResponsiveContainer,
 } from 'recharts'
 
@@ -20,17 +24,56 @@ export function Chart(props: ChartPropType) {
   return (
     <div
       className={[
-        'p-8 shadow border-2 border-secondary-90 bg-surface rounded-lg',
+        'p-4 pb-12 shadow border-2 border-secondary-90 bg-surface rounded-lg',
         props.className,
       ].join(' ')}
     >
+      <div className="mb-2 ml-[5%] pl-8 flex items-center">
+        <div className="py-[2px] px-2 rounded-full bg-neutral-95 flex items-center mr-4">
+          <div
+            onClick={() => {}}
+            className="w-[28px] h-[28px] hover:bg-neutral-90 rounded-full flex justify-between items-center"
+          >
+            <VerticalAlignBottomRoundedIcon
+              sx={{
+                fontSize: '20px',
+                transform: 'rotate(-90deg)',
+                margin: 'auto',
+              }}
+            ></VerticalAlignBottomRoundedIcon>
+          </div>
+          <div
+            onClick={() => {}}
+            className="w-[28px] h-[28px] hover:bg-neutral-90 rounded-full flex justify-between items-center"
+          >
+            <ExpandRoundedIcon
+              sx={{
+                fontSize: '20px',
+                transform: 'rotate(90deg)',
+                margin: 'auto',
+              }}
+            ></ExpandRoundedIcon>
+          </div>
+        </div>
+        <div className="py-[2px] px-2 rounded-full bg-neutral-95 flex items-center">
+          <div className="w-[28px] h-[28px] hover:bg-neutral-90 rounded-full flex justify-between items-center">
+            <ScatterPlotRoundedIcon
+              sx={{ fontSize: '20px', margin: 'auto' }}
+            ></ScatterPlotRoundedIcon>
+          </div>
+          <div className="w-[28px] h-[28px] hover:bg-neutral-90 rounded-full flex justify-between items-center">
+            <ShowChartRoundedIcon
+              sx={{ fontSize: '20px', margin: 'auto' }}
+            ></ShowChartRoundedIcon>
+          </div>
+        </div>
+      </div>
       <ResponsiveContainer width="100%" height="100%" debounce={20}>
         <LineChart width={200} height={200}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey={props.XAxis.key} type="number" />
           <YAxis dataKey={props.YAxis.key} />
           <Tooltip />
-          <Legend />
           <Line
             dataKey={props.YAxis.key}
             data={props.data}
--- src/renderer/src/features/chart/components/ChartsArea.tsx
+++ src/renderer/src/features/chart/components/ChartsArea.tsx
@@ -1,4 +1,3 @@
-// import { Container } from '@mui/material'
 import { Chart } from './Chart'
 
 const series = [
