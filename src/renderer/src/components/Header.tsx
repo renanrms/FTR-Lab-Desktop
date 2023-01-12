6c006f534a2e6ba28001524d17aae527ed15ed8c
Renan Passos <renanpassos@poli.ufrj.br>
2023-01-11 21:55:42 -0300

    Ajusta cores da Header.

--- src/renderer/src/components/Header.tsx
+++ src/renderer/src/components/Header.tsx
@@ -6,20 +6,20 @@ import IconButton from '@mui/material/IconButton'
 
 export function Header() {
   return (
-    <div className="col-span-2 h-12 bg-primary-40 text-on-primary flex shrink-0 items-center justify-between">
+    <div className="col-span-2 h-12 bg-primary-50 flex shrink-0 items-center justify-between">
       <div className="mx-4 h-10 flex items-center justify-center">
-        <IconButton style={{ color: 'var(--md-sys-color-on-primary)' }}>
+        <IconButton style={{ color: 'var(--md-ref-palette-primary100)' }}>
           <DeviceHubOutlinedIcon></DeviceHubOutlinedIcon>
         </IconButton>
       </div>
       <div className="mx-4 h-10 flex items-center justify-center">
-        <IconButton style={{ color: 'var(--md-sys-color-on-primary)' }}>
+        <IconButton style={{ color: 'var(--md-ref-palette-primary100)' }}>
           <AddchartOutlinedIcon></AddchartOutlinedIcon>
         </IconButton>
-        <IconButton style={{ color: 'var(--md-sys-color-on-primary)' }}>
+        <IconButton style={{ color: 'var(--md-ref-palette-primary100)' }}>
           <DownloadOutlinedIcon></DownloadOutlinedIcon>
         </IconButton>
-        <IconButton style={{ color: 'var(--md-sys-color-on-primary)' }}>
+        <IconButton style={{ color: 'var(--md-ref-palette-primary100)' }}>
           <DeleteOutlinedIcon></DeleteOutlinedIcon>
         </IconButton>
       </div>
