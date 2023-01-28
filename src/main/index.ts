da936295f4e7becc047b243d9cd15461d8e12318
Renan Passos <renanpassos@poli.ufrj.br>
2023-01-28 20:09:15 -0300

    Rascunho do tratamento de dispositivos e comunicação de main para renderer.
    - Adiciona lib multicast-dns

--- src/main/index.ts
+++ src/main/index.ts
@@ -4,6 +4,38 @@ import path from 'node:path'
 
 import { createWindow } from './createWindow'
 import './ipcHandlers/configure'
+import { State } from './utils/State'
+import { DeviceInfo } from '@shared/types/ipc'
+import { search, startListener } from './devicesHandlers/configure'
+
+const devices = new State<Array<DeviceInfo>>([])
+
+// console.log(devices.getState())
+
+devices.setState([
+  {
+    id: 'ff-ff-ff-ff-ff-ff',
+    name: 'Mecânica',
+    capabilities: ['Photogate', 'Distância'],
+    network: {
+      MACAddress: 'ff-ff-ff-ff-ff-ff',
+    },
+  },
+  {
+    id: 'ee-ee-ee-ee-ee-ee',
+    name: 'Termologia',
+    capabilities: ['Temperatura', 'Pressão'],
+    network: {
+      MACAddress: 'ee-ee-ee-ee-ee-ee',
+    },
+  },
+])
+
+// console.log(devices.getState())
+
+startListener(devices)
+
+search()
 
 if (process.platform === 'darwin') {
   app.dock.setIcon(path.resolve(__dirname, 'icon.png'))
