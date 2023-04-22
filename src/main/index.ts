e50cea0b96620cd59fd9ae042d5808dbb62e06c7
Renan Passos <renanpassos@poli.ufrj.br>
2023-04-21 23:07:49 -0300

    Faz um teste de conexão que consegue conectar.

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,12 +1,15 @@
 import { electronApp, optimizer } from '@electron-toolkit/utils'
 import { app, BrowserWindow } from 'electron'
+import net from 'node:net'
 import path from 'node:path'
 
-import { createWindow } from './createWindow'
-import './ipcHandlers/configure'
-import { State } from './utils/State'
 import { Device } from '@shared/types/Device'
+
+import { createWindow } from './createWindow'
 import { DevicesController } from './devices'
+import { State } from './utils/State'
+
+import './ipcHandlers/configure'
 
 const devicesState = new State<Array<Device>>([])
 
@@ -32,6 +35,14 @@ app.whenReady().then(() => {
   app.on('activate', function () {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
+
+  const client = new net.Socket()
+  console.log('socket')
+  client.connect({ port: 3333, host: '192.168.1.2' })
+  client.on('data', (data) => {
+    console.log(data.toString('utf-8'))
+  })
+  client.write('Hello, ESP32!!')
 })
 
 app.on('window-all-closed', () => {
