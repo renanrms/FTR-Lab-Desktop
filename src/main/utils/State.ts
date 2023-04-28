b4ce6d2ed4f01f3bff06345046c310b0f81c0529
Renan Passos <renanpassos@poli.ufrj.br>
2023-04-27 23:49:21 -0300

    Deixa de usar copia no acesso ao estado na classe State.

--- src/main/utils/State.ts
+++ src/main/utils/State.ts
@@ -1,4 +1,4 @@
-import { copy } from './copy'
+// import { copy } from './copy'
 
 // TODO: verificar se existe problema de concorrência com esta classe. Caso positivo implementar trava.
 
@@ -6,14 +6,14 @@ export class State<T> {
   private state: T
 
   constructor(initialState: T) {
-    this.state = copy(initialState)
+    this.state = initialState
   }
 
   public get() {
-    return copy(this.state)
+    return this.state
   }
 
   public set(newState: T) {
-    this.state = copy(newState)
+    this.state = newState
   }
 }
