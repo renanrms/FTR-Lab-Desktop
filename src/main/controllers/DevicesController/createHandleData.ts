497d4d47152e48d28afb3ea0a807628459f110b2
Renan Passos <renanpassos@poli.ufrj.br>
2023-08-20 16:53:36 -0300

    Remove TODO sobre problema de concorrência.
    - O possível problema foi estudado e parece que realmente não existia.

--- src/main/controllers/DevicesController/createHandleData.ts
+++ src/main/controllers/DevicesController/createHandleData.ts
@@ -6,25 +6,17 @@ export function createHandleData(
   handleDeviceMessage: (message: string, id: string) => Promise<void>,
 ) {
   return async (data: Buffer) => {
-    /**
-     * TODO: Resolver problema de concorrência. Quando a frequência de envio é muito alta,
-     * fica mais de uma instância desta função na memória e o buffer compartilhado gera um
-     * problema de concorrência.
-     */
     console.log(`<< ${id} | Data (${data.length} bytes)`)
     // console.log(data.toString('utf-8'))
 
     let messages: string[] = []
 
-    // Seria interessante proteger o buffer com um lock.
     connection.buffer += data.toString('utf-8')
     messages = connection.buffer.split(/\n{1,2}/)
     connection.buffer = messages.pop() || ''
 
-    messages = messages.filter((message) => message.length > 0)
-
     for (const message of messages) {
-      await handleDeviceMessage(message, id)
+      if (message) await handleDeviceMessage(message, id)
     }
   }
 }
