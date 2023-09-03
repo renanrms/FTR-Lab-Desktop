235b279aba27333b56eb6da358ffc22d326f0c44
Renan Passos <renanpassos@poli.ufrj.br>
2023-09-03 00:58:00 -0300

    Feat: Adiciona abas no AboutModal com @mui/lab

--- src/renderer/src/theme/muiTheme.js
+++ src/renderer/src/theme/muiTheme.js
@@ -1,8 +1,19 @@
 import { createTheme } from '@mui/material/styles'
 
+function getPreferredColorScheme() {
+  if (window.matchMedia) {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      return 'dark'
+    } else {
+      return 'light'
+    }
+  }
+  return 'light'
+}
+
 const theme = createTheme({
   palette: {
-    mode: 'light',
+    mode: getPreferredColorScheme(),
     primary: {
       main: '#006c51',
     },
