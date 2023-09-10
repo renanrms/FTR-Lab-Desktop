9266afb7a47b5c0e4938db1e2d55f64349635691
Renan Passos <renanpassos@poli.ufrj.br>
2023-09-09 22:31:28 -0300

    Limpa alterações parciais no deviceCard.

--- src/renderer/src/features/devices/components/DeviceCard.tsx
+++ src/renderer/src/features/devices/components/DeviceCard.tsx
@@ -2,13 +2,11 @@ import AccessTimeIcon from '@mui/icons-material/AccessTime'
 // import PauseIcon from '@mui/icons-material/Pause'
 import PauseIcon from '@mui/icons-material/PauseOutlined'
 import PlayIcon from '@mui/icons-material/PlayArrowOutlined'
-import { Button, Chip, IconButton, Tooltip, Typography } from '@mui/material'
-import Switch from '@mui/material/Switch'
+import { Button, Tooltip } from '@mui/material'
 import { useMutation } from '@tanstack/react-query'
 import { twMerge } from 'tailwind-merge'
 
 import { quantities } from '@renderer/constants/quantities'
-import { preferredColorScheme } from '@renderer/theme/muiTheme'
 import { Device } from '@shared/types/Device'
 
 import { toggleConnection } from '../services/toggleConnection'
@@ -22,13 +20,6 @@ interface DeviceCardProps {
 export function DeviceCard(props: DeviceCardProps) {
   const connectionMutation = useMutation({ mutationFn: toggleConnection })
 
-  // props.device.available = true
-  // props.device.connected = true
-  // props.device.battery = {
-  //   charging: false,
-  //   level: 60,
-  // }
-
   return (
     <div
       className={twMerge(
@@ -36,25 +27,6 @@ export function DeviceCard(props: DeviceCardProps) {
         !props.device.available && !props.device.connected && 'opacity-60',
       )}
     >
-      {/* <div className="h-8 flex items-center">
-        <Switch
-          icon={
-            <div className="rounded-full bg-white shadow-md border border-neutral-90">
-              <PlayArrowIcon />
-            </div>
-          }
-          checkedIcon={<PauseIcon />}
-          onClick={() => {
-            connectionMutation.mutate(props.device)
-          }}
-          checked={!!props.device.connected}
-          disabled={
-            (!props.device.available && !props.device.connected) ||
-            connectionMutation.isLoading
-          }
-        />
-      </div> */}
-
       <div className="w-full flex items-center justify-between">
         <div>
           <p className="grow text-md">{props.device.name}</p>
@@ -100,15 +72,15 @@ export function DeviceCard(props: DeviceCardProps) {
           const quantity = quantities[sensor.quantity]
           return (
             <div
-              className="flex flex-nowrap w-fit h-fit divide-x rounded-2xl border bg-neutral-95 dark:bg-neutral-20 border-neutral-90 dark:border-neutral-25 divide-neutral-90 dark:divide-neutral-25"
+              className="flex flex-nowrap w-fit h-fit divide-x rounded-2xl border bg-neutral-95 dark:bg-neutral-20 border-neutral-90 dark:border-neutral-25 divide-neutral-90 dark:divide-neutral-25 text-neutral-30 dark:text-neutral-90"
               key={index}
             >
-              <div className="text-sm px-2">
+              <div className="text-sm px-2 py-0.5">
                 {quantity?.name || sensor.quantity}
               </div>
 
               {quantity?.defaultUnit.symbol && (
-                <div className="text-sm px-2">
+                <div className="text-sm px-2 py-0.5">
                   {quantity.defaultUnit.symbol}
                 </div>
               )}
@@ -119,23 +91,19 @@ export function DeviceCard(props: DeviceCardProps) {
 
       <div className="w-full h-8 flex justify-center items-center">
         <Button
-          // variant="outlined"
           color={props.device.connected ? 'error' : 'primary'}
           onClick={() => {
             connectionMutation.mutate(props.device)
           }}
           startIcon={props.device.connected ? <PauseIcon /> : <PlayIcon />}
           sx={{
-            // color: 'white',
             paddingX: '16px',
             paddingY: '4px',
-            // backgroundColor:
-            // preferredColorScheme === 'dark' ? '#6665' : '#0005',
             borderRadius: '2rem',
-            backgroundColor: props.device.connected ? '#d32f2f10' : '#00876717',
+            backgroundColor: props.device.connected ? '#00000010' : '#00876717',
             ':hover': {
               backgroundColor: props.device.connected
-                ? '#d32f2f18'
+                ? '#00000018'
                 : '#00876722',
             },
             textTransform: 'capitalize',
@@ -149,39 +117,6 @@ export function DeviceCard(props: DeviceCardProps) {
             {props.device.connected ? 'Parar' : 'Iniciar'}
           </span>
         </Button>
-
-        {/* <Chip
-          variant="outlined"
-          color="primary"
-          icon={
-            props.device.connected ? (
-              <PauseIcon color="inherit" />
-            ) : (
-              <PlayIcon color="inherit" />
-            )
-          }
-          // size="small"
-          label={
-            <span className="w-12">
-              {props.device.connected ? 'Parar' : 'Iniciar'}
-            </span>
-          }
-          // sx={{
-          //   padding: '2px',
-          //   color: 'white',
-          //   backgroundColor:
-          //     preferredColorScheme === 'dark'
-          //       ? 'var(--md-sys-color-primary-container)'
-          //       : 'var(--md-ref-palette-primary60)',
-          // }}
-          onClick={() => {
-            connectionMutation.mutate(props.device)
-          }}
-          disabled={
-            (!props.device.available && !props.device.connected) ||
-            connectionMutation.isLoading
-          }
-        /> */}
       </div>
     </div>
   )
