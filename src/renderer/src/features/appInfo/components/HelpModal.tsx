c216cfd7bcc3192017410e8fd06aa949cae32109
Renan Passos <renanpassos@poli.ufrj.br>
2023-09-10 22:06:50 -0300

    Feat: Adiciona instruções de configuração do dispositivo.

--- src/renderer/src/features/appInfo/components/HelpModal.tsx
+++ src/renderer/src/features/appInfo/components/HelpModal.tsx
@@ -36,7 +36,7 @@ export function HelpModal(props: HelpModalProps) {
         },
       }}
     >
-      <div className="relative flex flex-col items-center h-fit min-h-[420px] max-h-[95%] w-fit min-w-[800px] max-w-[1000px] bg-background dark:bg-neutral-10 rounded-lg text-on-background ">
+      <div className="relative flex flex-col items-center h-fit min-h-[420px] max-h-[95%] w-[95%] max-w-[850px] bg-background dark:bg-neutral-10 rounded-lg text-on-background ">
         <Box
           className="flex justify-center w-full"
           sx={{ borderBottom: 1, borderColor: 'divider' }}
@@ -55,6 +55,11 @@ export function HelpModal(props: HelpModalProps) {
             <Tab
               label="Instruções de Uso"
               value="instructions"
+              sx={{ fontSize: '1rem', textTransform: 'none' }}
+            />
+            <Tab
+              label="Configuração do Dispositivo"
+              value="configuration"
               sx={{ fontSize: '1rem', textTransform: 'none' }}
             />
           </Tabs>
@@ -130,7 +135,7 @@ export function HelpModal(props: HelpModalProps) {
         >
           <div className="w-full flex justify-center">
             <img
-              className="rounded-lg border w-[900px]"
+              className="rounded-lg border border-neutral-80 dark:border-neutral-20 w-[850px]"
               src={ScreenCaptureHighlighted}
               alt="Captura de tela com destaques em elementos mais importantes"
             />
@@ -141,8 +146,16 @@ export function HelpModal(props: HelpModalProps) {
               Se houver dispositivos encontrados na rede e medições, a tela terá
               estes elementos principais: os dispositivos listados na esquerda e
               os gráficos na área principal da tela. Caso ainda não esteja vendo
-              nenhum dispositivo para uso, veja as{' '}
-              <span className="font-medium">instruções do dispositivo</span>.
+              nenhum dispositivo para uso, veja as instruções de{' '}
+              <a
+                className="underline hover:text-primary-50 hover:dark:text-primary-80"
+                onClick={() => {
+                  setValue('configuration')
+                }}
+              >
+                Configuração do Dispositivo
+              </a>
+              .
             </p>
             <p>
               Cada dispositivo é mostrado como um card, com o nome e seu número
@@ -177,6 +190,62 @@ export function HelpModal(props: HelpModalProps) {
               recebidos do sensor serão salvos no arquivo. Além disso há o botão
               de <span className="font-medium text-primary">Ajuda</span> (5).
             </p>
+          </div>
+        </TabPanel>
+        <TabPanel
+          className="p-4 overflow-y-auto w-full"
+          value={value}
+          index="configuration"
+        >
+          <div className="my-6 px-4 w-full flex flex-col gap-4">
+            <p className="py-4 px-8 bg-tertiary-95 dark:bg-tertiary-20 rounded-xl">
+              Pré-requisito: ter acesso a uma rede WiFi no local, em que o
+              computador e o dispositivo possam se conectar. É possível utilizar
+              com o computador conectado por cabo, se for a mesma rede, porém
+              isso dependerá de configurações da rede local.
+            </p>
+            <p>
+              Caso o dispositivo FTRLab ainda não esteja configurado ou não
+              esteja aparecendo no software, você poderá seguir este passo a
+              passo para configurá-lo.
+            </p>
+            <ol
+              className="w-full flex flex-col gap-2"
+              style={{ listStyleType: 'decimal' }}
+            >
+              <li className="ml-8 mt-1">
+                Pressione o botão de Reiniciar e aguarde o LED começar a piscar.
+              </li>
+              <li className="ml-8 mt-1">
+                Se o LED piscar lentamente (0,5 segundo ON, seguido de 0,5
+                segundo OFF), está no modo de configuração pois não conhece
+                nenhuma rede WiFi do local. Utilize o Aplicativo de celular ESP
+                TOUCH para configurar. Você deverá conectar seu celular na rede
+                desejada, abrir o aplicativo, digitar a senha da rede e apertar
+                o botão para configurar.
+              </li>
+              <li className="ml-8 mt-1">
+                Se o LED piscar rapidamente (Fica ON, passa pelo estado OFF por
+                um curto intervalo de tempo), está sincronizando seu relógio, o
+                que demora no máximo 90 segundos, mas tipicamente menos de 10
+                segundos. Aguarde.
+              </li>
+              <li className="ml-8 mt-1">
+                Se o LED ficar ON de forma constante, está funcionando conectado
+                à rede configurada. Conecte o computador à mesma rede. Verifique
+                se existem redes WiFi de gerações diferentes com o mesmo nome
+                (como WiFi 2.4GHz e 5GHz). Nestes casos os dois sistemas podem
+                não conseguir se comunicar.
+              </li>
+              <li className="ml-8 mt-1">
+                Se o LED ficar ON de forma constante, mas você deseja configurar
+                uma nova rede, mantenha o botão de configuração pressionado e
+                pressione o botão de reiniciar, enquanto isso. Pare de segurar o
+                botão de configuração quando dispositivo entrar no modo de
+                configuração. Tendo feito isso você pode seguir novamente do
+                passo 2 em diante.
+              </li>
+            </ol>
           </div>
         </TabPanel>
       </div>
