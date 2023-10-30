7fa3d2bcb9365436f6c7565176338cd2187320fd
Renan Passos <renanpassos@poli.ufrj.br>
2023-10-29 23:51:04 -0300

    Feat: Muda local onde é armazenado arquivo do sqlite para pasta de dados.

--- src/main/database/db.ts
+++ src/main/database/db.ts
@@ -1,8 +1,10 @@
+import { app } from 'electron'
+import path from 'path'
 import { Sequelize } from 'sequelize'
 
 export const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: './FTRLab-db.sqlite', // TODO: Em produção, colocar na pasta de dados adequada para não ficar salvo ao lado do executável
+  storage: path.join(app.getPath('userData'), 'FTRLab-db.sqlite'),
   database: 'ftrlab-db',
   logging: false, // TODO: Implementar log para comunicação com o banco
 })
