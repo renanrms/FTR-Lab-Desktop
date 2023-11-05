cc642d5b904a77244ea768c6927538e16482faeb
Renan Passos <renanpassos@poli.ufrj.br>
2023-11-05 20:41:18 -0300

    Feat: Volta a r os dados na pasta do projeto quando está em ambiente de desenvolvimento.

--- src/main/database/db.ts
+++ src/main/database/db.ts
@@ -2,10 +2,18 @@ import { app } from 'electron'
 import path from 'path'
 import { Sequelize } from 'sequelize'
 
+/**
+ * O paths de armazenamento dos dados em produção podem ser:
+ * Windows -> C:\Users\<user>\AppData\Roaming\FTRLab\FTRLab-db.sqlite
+ * Linux ->   /home/<user>/.config/FTRLab/FTRLab-db.sqlite
+ */
+
 export const sequelize = new Sequelize({
   dialect: 'sqlite',
-  // Windows -> C:\Users\<user>\AppData\Roaming\FTRLab\FTRLab-db.sqlite
-  storage: path.join(app.getPath('userData'), 'FTRLab-db.sqlite'),
+  storage: path.join(
+    process.env.NODE_ENV === 'development' ? './' : app.getPath('userData'),
+    'FTRLab-db.sqlite',
+  ),
   database: 'ftrlab-db',
   logging: false, // TODO: Implementar log para comunicação com o banco
 })
