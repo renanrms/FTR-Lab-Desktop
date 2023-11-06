e25a167340dcf343000daa909146c6d3700cf24c
Renan Passos <34728048+renanrms@users.noreply.github.com>
2023-11-05 23:14:47 -0300

    Style: Comentário indicando possível caminho para a database no windows.

--- src/main/database/db.ts
+++ src/main/database/db.ts
@@ -4,6 +4,7 @@ import { Sequelize } from 'sequelize'
 
 export const sequelize = new Sequelize({
   dialect: 'sqlite',
+  // Windows -> C:\Users\<user>\AppData\Roaming\FTRLab\FTRLab-db.sqlite
   storage: path.join(app.getPath('userData'), 'FTRLab-db.sqlite'),
   database: 'ftrlab-db',
   logging: false, // TODO: Implementar log para comunicação com o banco
